71a7ae846d40c6a416226daf584bf81d3533f7ed
Beatriz <BEATRIZ.CRUZ84@ETEC.SP.GOV.BR>
2024-02-23 11:58:57 +0000

    save1

--- app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -12,4 +12,17 @@ return this.idade >= 18;
   public void setNome (String nome) {
    this.nome = nome;
   } 
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+
+  }
+
+  public int getIdade() {
+    return this.idade;
+  }
 }
